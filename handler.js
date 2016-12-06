324752c7afeef616e72d1332906054d01197e0d9
vineetsaini <vsaini@loyaltymethods.com>
2016-12-06 12:05:02 -0700

    lets do it 2

--- handler.js
+++ handler.js
@@ -23,7 +23,7 @@ function createResponse(){
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Code Deploy via Pipeline'
+      message: 'Go Code Deploy via Pipeline 2'
     })
   };
   return response;
