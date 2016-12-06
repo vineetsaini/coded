1dd1088b671869eefc5175f9603c14be0a379dab
vineetsaini <vsaini@loyaltymethods.com>
2016-12-06 11:58:20 -0700

    lets do it

---
+++ handler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports.hello =(event, context, callback)=>{
+  
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     message: 'Go Code Deploy via Pipeline',
+  //     input: event,
+  //   }),
+  // };
+  const response = createResponse();
+ 
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+
+function createResponse(){
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Code Deploy via Pipeline'
+    })
+  };
+  return response;
+}
+
+exports.createResponse = createResponse;
+
---
+++ test/test.js
@@ -0,0 +1,52 @@
+
+'use strict';
+
+const expect = require('chai').expect
+ , handler = require('../handler');
+
+// describe('Some Lambda Func', function() {
+//   let ctx;
+  
+//   before(function() {
+//     ctx = new MockContext();
+//   });
+  
+//   describe('When Some param is greater than zero', function() {
+//     before(function(done) {
+//       SomeLambda.hello({someParam: 10}, ctx, done)
+//     });
+    
+//     it('should succeed with Yay', function() {
+//       expect(done.response.statusCode).to.equal(200)
+//     });
+    
+//     after(function() {
+//       //reset counters
+//       ctx.reset();
+//     })
+    
+//   })
+// });
+describe('createResponse', function() {
+        let response;
+        before(function(){
+                response = handler.createResponse();
+        });
+        it("testResponseCode",function(){
+                
+                expect(response.statusCode).to.equal(200);
+        });
+        
+});
+
+// function add(a,b){
+//         return a+b;
+// };
+
+// describe('Add', function() {
+//         it("Addtest",function(){
+//                 let response = add(4,3);
+//                 expect(response).to.equal(7);
+//         });
+// });
+
